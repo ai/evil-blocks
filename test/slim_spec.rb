24cb6675d71f642a47125243a44d2a3db34ea510
Andrey Sitnik <andrey@sitnik.ru>
2014-06-02 17:04:32 +0800

    Use expect syntax in RSpec

--- test/slim_spec.rb
+++ test/slim_spec.rb
@@ -6,22 +6,23 @@ EvilBlocks.install_to_slim!
 describe 'Slim hack' do
 
   it 'adds @dataRole alias' do
-    Slim::Template.new { '.name@nameField' }.render.should ==
-      '<div class="name" data-role="nameField"></div>'
+    expect(Slim::Template.new { '.name@nameField' }.render).to eq(
+      '<div class="name" data-role="nameField"></div>')
   end
 
   it 'supports multiple roles' do
-    Slim::Template.new { '@a@b' }.render.should == '<div data-role="a b"></div>'
+    expect(Slim::Template.new { '@a@b' }.render).to eq(
+      '<div data-role="a b"></div>')
   end
 
   it 'adds @@dataBlock alias' do
-    Slim::Template.new { '.name@@control' }.render.should ==
-      '<div class="name" data-block="control"></div>'
+    expect(Slim::Template.new { '.name@@control' }.render).to eq(
+      '<div class="name" data-block="control"></div>')
   end
 
   it 'supports multiple bloks' do
-    Slim::Template.new { '@@a@@b' }.render.should ==
-      '<div data-block="a b"></div>'
+    expect(Slim::Template.new { '@@a@@b' }.render).to eq(
+      '<div data-block="a b"></div>')
   end
 
 end
